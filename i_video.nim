b3c42e5564f3f691e13d3a019b70da497419e5b6
mvenditto <matteo.venditto@studio.unibo.it>
2019-09-14 21:56:57 +0200

    dirty migration to sdl2 + upscaling

--- i_video.nim
+++ i_video.nim
@@ -8,18 +8,34 @@
 import os
 {.passC:"-I" & currentSourcePath().splitPath.head .}
 
-import sdl
+import sdl2/sdl
 import doomdef
 import d_event
 
-var screen: PSurface
 var firsttimee = true
 var X_width: cint
 var X_height: cint
 const multiply: cint = 1
 var colors: array[256, sdl.Color]
 
-const SCREENWIDTH: cint = 320
+const
+  Title = "nimrod00m"
+  WindowFlags = 0
+  RendererFlags = sdl.RendererAccelerated
+
+
+type
+  App = ref AppObj
+  AppObj = object
+    window*: sdl.Window # Window pointer
+    renderer*: sdl.Renderer # Rendering state pointer
+
+var app = App(window: nil, renderer: nil)
+var surface: Surface
+var psurface: Surface
+var texture: Texture
+
+const SCREENWIDTH: cint = 320 
 const SCREENHEIGHT: cint = 200
 
 proc I_Quit {.importc, header:"i_system.h", noconv.}
@@ -27,7 +43,6 @@ proc I_Error(error: cstring) {.importc, header:"i_system.h", noconv.}
 proc D_PostEvent*(ev: ptr event_t) {.importc, header:"d_main.h", noconv.}
 
 var screens* {.importc, header:"v_video.h".}: array[5, ptr UncheckedArray[byte]]
-
 var gammatable {.importc, header:"v_video.h".}: array[5, array[256, byte]]
 var usegamma {.importc, header:"v_video.h".}: cint
 
@@ -43,21 +58,42 @@ proc I_InitGraphics*(): void {.exportc.} =
     X_width = SCREENWIDTH * multiply;
     X_height = SCREENHEIGHT * multiply;
 
-    if init(INIT_EVERYTHING) == -1:
+    if init(sdl.InitEverything) == -1:
         I_Error("SDL initializing error")
     
-    screen = setVideoMode(SCREENWIDTH, SCREENHEIGHT, 8, SWSURFACE)
+    app.window = sdl.createWindow(
+        Title,
+        sdl.WindowPosUndefined,
+        sdl.WindowPosUndefined,
+        X_width * 2,
+        X_height * 2,
+        WindowFlags
+    )
 
-    if screen == nil:
-        I_Error("SDL setting video mode error")
+    if app.window == nil:
+        I_Error("ERROR: Can't create window: ")
 
-    var sin = screen == nil
-    echo "> " & $sin
-    screens[0] = cast[ptr UncheckedArray[byte]](screen.pixels)
+    # Create renderer
+    app.renderer = sdl.createRenderer(app.window, -1, RendererFlags)
+    if app.renderer == nil:
+        I_Error("ERROR: Can't create renderer: ")
+    
+    discard sdl.setHint(sdl.HINT_RENDER_SCALE_QUALITY, "nearest")
+    discard app.renderer.renderSetLogicalSize(320, 200)
+
+    surface = sdl.createRGBSurface(0, 320, 200, 8, 0, 0, 0, 0)
+    psurface = sdl.createRGBSurface(0, 320, 200, 32, 0, 0, 0, 0)
+
+    texture = app.renderer.createTexture(
+        sdl.PIXELFORMAT_RGBA8888, sdl.TEXTUREACCESS_STREAMING, 320, 200);
+
+
+    screens[0] = cast[ptr UncheckedArray[byte]](surface.pixels)
     
 proc I_ShutdownGraphics*(): void {.exportc.} =
-    #echo "> I_ShutdownGraphics"
-    freeSurface(screen)
+    app.renderer.destroyRenderer()
+    app.window.destroyWindow()
+    sdl.quit()
 
 proc I_UpdateNoBlit*(): void {.exportc.} =
     #echo "> I_UpdateNoBlit"
@@ -65,14 +101,25 @@ proc I_UpdateNoBlit*(): void {.exportc.} =
 
 proc I_FinishUpdate*(): void {.exportc.} =
     #echo "> I_FinishUpdate"
-    discard flip(screen)
+    discard sdl.blitSurface(surface, nil, psurface, nil)
+    var pitch: cint
+    var pixels: pointer
+
+    discard sdl.lockTexture(texture, nil, addr(pixels), addr(pitch))
+    discard sdl.convertPixels(320, 200, 
+        psurface.format.format, 
+        psurface.pixels, psurface.pitch, 
+        sdl.PIXELFORMAT_RGBA8888, pixels, pitch)
+    sdl.unlockTexture(texture)
+    discard app.renderer.renderCopy(texture, nil, nil)
+    app.renderer.renderPresent();
 
 template `\\`(x: untyped): untyped = cast[ptr type(x[0])](addr x)
 
 proc I_SetPalette*(palette: ptr byte): void {.exportc.} =
     #echo "> I_SetPalette"
 
-    echo $palette[]
+    # echo $palette[]
 
     # var palette = cast[ptr UncheckedArray[byte]](palette_void_ptr)
     var pi: int = 0
@@ -98,7 +145,8 @@ proc I_SetPalette*(palette: ptr byte): void {.exportc.} =
         pp = inc()
         colors[i].b = (c shl 8) + c
     
-    discard screen.setColors(\\colors, 0, 256)
+    # discard screen.setColors(\\colors, 0, 256)
+    discard sdl.setPaletteColors(surface.format.palette, addr(colors[0]), 0, 256)
     
     
 proc I_StartFrame*() {.exportc.} = 
@@ -109,12 +157,10 @@ proc I_ReadScreen*(src: ptr UncheckedArray[byte]) {.exportc.} =
     #echo "> I_ReadScreen"
     copyMem(src, screens[0], SCREENWIDTH * SCREENHEIGHT)
 
-proc xlatekey*(event: PEvent): cint {.exportc.} =
-
-    var ke = evKeyboard(event);
-    var rc: cint = ke.keysym.sym
-    case rc:
-        of K_LEFT:
+proc xlatekey*(e: Event): cint {.exportc.} =
+    var rc: cint
+    case e.key.keysym.sym:
+        of sdl.K_LEFT:
             rc = KEY_LEFTARROW
         of K_RIGHT:
             rc = KEY_RIGHTARROW
@@ -164,38 +210,39 @@ proc xlatekey*(event: PEvent): cint {.exportc.} =
             rc = KEY_RSHIFT
         of K_LCTRL, K_RCTRL:
             rc = KEY_RCTRL
-        of K_LALT, K_LMETA, K_RALT, K_RMETA:
+        of K_LALT, K_LGUI, K_RALT, K_RGUI:
             rc = KEY_RALT  
         else: 
             if rc >= 65 and rc <= 90: # [a-zA-Z]
                 rc = rc + 32
+                
     return rc
 
-proc I_ProcessEvent*(sdl_event: PEvent) {.exportc.} =
+proc I_ProcessEvent*(e: Event) {.exportc.} =
     var event: event_t
-    case sdl_event.kind:
-        of KEYDOWN:
+    case e.kind:
+        of sdl.KeyDown:
             event.`type` = ev_keydown
-            event.data1 = xlatekey(sdl_event)
+            event.data1 = xlatekey(e)
             discard
-        of KEYUP:
+        of sdl.KeyUp:
             event.`type` = ev_keyup
-            event.data1 = xlatekey(sdl_event)
-            discard;
-        of QUITEV:
-            I_Quit();    
+            event.data1 = xlatekey(e)
+        of sdl.Quit:
+            I_Quit();
         else:
             return
+
     D_PostEvent(addr(event))
 
 proc I_StartTic() {.exportc.} =
     #echo "> I_StartTic"
-    if screen == nil:
+    if app.window == nil:
         echo "> screen is nil"
         return
 
-    var event: sdl.Event
-    while pollEvent(addr(event)) != 0:
-        #echo " >process event"  
-        I_ProcessEvent(addr(event))   
+    var e: sdl.Event
+
+    while sdl.pollEvent(addr(e)) != 0:
+        I_ProcessEvent(e)
     
