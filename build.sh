10b772d1be555e6bc147980788ce5c692dae012c
mvenditto <matteo.venditto@studio.unibo.it>
2019-09-21 23:03:25 +0200

    exit if nim compilation fails

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ BUILDDIR="/vagrant/"
 NIMDIR=$HOME/.cache/nim/${LIBNAME}_d
 rm $NIMDIR/*
 nim c -f --cpu:i386 --os:linux --noMain --app:staticlib --header -d:nimDebugDlOpen --genScript "$LIBNAME.nim"
+retval=$?
+if [ $retval -ne 0 ]; then
+    exit
+fi
 cd $NIMDIR
 sh "compile_${LIBNAME}.sh"
 cp "${LIBNAME}.h" $BUILDDIR
